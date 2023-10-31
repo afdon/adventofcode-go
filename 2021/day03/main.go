2dc08972a53946c3a437c2ea93582b87df6a31f1
afdon <afdonovan@alumni.princeton.edu>
2023-10-31 00:19:07 -0700

    part 1 done

--- 2021/day03/main.go
+++ 2021/day03/main.go
@@ -84,12 +84,12 @@ func epsilon(lines []string) []byte {
 func binaryToDecimal(num int) int {
 	var remainder int
 	index := 0
-	decimalNum := 0
+	dec := 0
 	for num != 0 {
 		remainder = num % 10
 		num = num / 10
-		decimalNum = decimalNum + remainder*int(math.Pow(2, float64(index)))
+		dec = dec + remainder*int(math.Pow(2, float64(index)))
 		index++
 	}
-	return decimalNum
+	return dec
 }
