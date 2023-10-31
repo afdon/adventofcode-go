5ca167caedc7accc8490c77aef59cbba1f768006
afdon <afdonovan@alumni.princeton.edu>
2023-10-31 00:08:01 -0700

    troubleshoot

--- 2021/day03/archive/main.go
+++ 2021/day03/archive/main.go
@@ -3,14 +3,16 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"math"
 	"strconv"
 	"strings"
 )
 
-//go:embed input.txt
+// go:embed input.txt
 var input string
 
 func main() {
+
 	lines := strings.Split(strings.TrimSpace(input), "\n")
 
 	gamma := gamma(lines)
@@ -19,43 +21,51 @@ func main() {
 	gammaInt, _ := strconv.Atoi(string(gamma))
 	epsilonInt, _ := strconv.Atoi(string(epsilon))
 
-	power := gammaInt * epsilonInt
+	power := binaryToDecimal(gammaInt) * binaryToDecimal(epsilonInt)
 
 	fmt.Println(gammaInt, epsilonInt, power)
+
 }
 
 func gamma(lines []string) []byte {
-	gamma := make([]byte, len(lines[0]))
+
+	var gamma []byte
 
 	for i := 0; i < len(lines[0]); i++ {
+
 		ones := 0
 		zeroes := 0
 
 		for j := 0; j < len(lines); j++ {
+			fmt.Print(lines[j])
 			if string(lines[j][i]) == "0" {
 				zeroes++
+				// fmt.Print(zeroes)
 			}
 			if string(lines[j][i]) == "1" {
 				ones++
+				// fmt.Print(ones)
 			}
 		}
 
 		if zeroes > ones {
-			gamma[i] = '0'
+			gamma[i] = 0
 		}
 		if ones > zeroes {
-			gamma[i] = '1'
+			gamma[i] = 1
 		}
 	}
 
-	fmt.Println(string(gamma))
+	fmt.Println(gamma)
 	return gamma
 }
 
 func epsilon(lines []string) []byte {
-	epsilon := make([]byte, len(lines[0]))
+
+	var epsilon []byte
 
 	for i := 0; i < len(lines[0]); i++ {
+
 		ones := 0
 		zeroes := 0
 
@@ -69,13 +79,26 @@ func epsilon(lines []string) []byte {
 		}
 
 		if zeroes > ones {
-			epsilon[i] = '0'
+			epsilon[i] = 0
 		}
 		if ones > zeroes {
-			epsilon[i] = '1'
+			epsilon[i] = 1
 		}
 	}
 
-	fmt.Println(string(epsilon))
+	fmt.Println(epsilon)
 	return epsilon
+}
+
+func binaryToDecimal(num int) int {
+	var remainder int
+	index := 0
+	decimalNum := 0
+	for num != 0 {
+		remainder = num % 10
+		num = num / 10
+		decimalNum = decimalNum + remainder*int(math.Pow(2, float64(index)))
+		index++
+	}
+	return decimalNum
 }
