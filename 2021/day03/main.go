7e791ce372f526bd5255538f65dcee3b2afe9d02
afdon <afdonovan@alumni.princeton.edu>
2023-11-01 00:18:36 -0700

    advent of code wrote day 09 functions

--- 2021/day03/main.go
+++ 2021/day03/main.go
@@ -4,7 +4,6 @@ import (
 	_ "embed"
 	"fmt"
 	"math"
-	"strconv"
 	"strings"
 )
 
@@ -12,21 +11,27 @@ import (
 var input string
 
 func main() {
-	lines := strings.Split(strings.TrimSpace(input), "\n")
+	raw := strings.Split(strings.TrimSpace(input), "\n")
 
-	gamma := gamma(lines)
-	epsilon := epsilon(lines)
+	lines := raw // copy the array by value, not reference (&raw)
 
-	gammaInt, _ := strconv.Atoi(string(gamma))
-	epsilonInt, _ := strconv.Atoi(string(epsilon))
+	rp := &raw
+	lp := &lines
+	fmt.Printf("%v, %v", &rp, &lp)
 
-	power := binaryToDecimal(gammaInt) * binaryToDecimal(epsilonInt)
+	// oxygen := oxygen(lines)
+	// co2 := co2(lines)
 
-	fmt.Println(gammaInt, epsilonInt, power)
+	// oxygenInt, _ := strconv.Atoi(string(oxygen))
+	// co2Int, _ := strconv.Atoi(string(co2))
+
+	// power := binaryToDecimal(oxygenInt) * binaryToDecimal(co2Int)
+
+	// fmt.Println(oxygenInt, co2Int, power)
 }
 
-func gamma(lines []string) []byte {
-	gamma := make([]byte, len(lines[0]))
+func tally(lines []string) []int {
+	tally := make([]int, len(lines[0]))
 
 	for i := 0; i < len(lines[0]); i++ {
 		ones := 0
@@ -42,19 +47,20 @@ func gamma(lines []string) []byte {
 		}
 
 		if zeroes > ones {
-			gamma[i] = '0'
-		}
-		if ones > zeroes {
-			gamma[i] = '1'
+			// keep all the elements with zeroes
+			tally[i] = '0'
+		} else {
+			// keep all the elements with ones
+			tally[i] = '1'
 		}
 	}
 
-	fmt.Println(string(gamma))
-	return gamma
+	fmt.Println(tally)
+	return tally
 }
 
-func epsilon(lines []string) []byte {
-	epsilon := make([]byte, len(lines[0]))
+func tallyco2(lines []string) []int {
+	tally := make([]int, len(lines[0]))
 
 	for i := 0; i < len(lines[0]); i++ {
 		ones := 0
@@ -70,15 +76,58 @@ func epsilon(lines []string) []byte {
 		}
 
 		if zeroes > ones {
-			epsilon[i] = '1'
-		}
-		if ones > zeroes {
-			epsilon[i] = '0'
+			tally[i] = '1'
+		} else {
+			tally[i] = '0'
 		}
 	}
 
-	fmt.Println(string(epsilon))
-	return epsilon
+	fmt.Println(tally)
+	return tally
+}
+
+func oxygen(lines []string, tally []byte) []byte {
+
+	oxygen := make([]byte, len(lines[0]))
+
+	filteredLines := lines
+
+	for i := 0; i < len(tally); i++ {
+		for j := 0; j < len(filteredLines); j++ {
+
+			if len(oxygen) == 1 {
+				return oxygen
+			}
+
+			if lines[j][i] != tally[i] {
+				// remove it from array.
+				filteredLines = append(filteredLines[:j-1], filteredLines[j+1:]...)
+			}
+		}
+	}
+	return oxygen
+}
+
+func co2(lines []string, tally []byte) []byte {
+
+	co2 := make([]byte, len(lines[0]))
+
+	filteredLines := lines
+
+	for i := 0; i < len(tally); i++ {
+		for j := 0; j < len(filteredLines); j++ {
+
+			if len(co2) == 1 {
+				return co2
+			}
+
+			if lines[j][i] != tally[i] {
+				// remove it from array.
+				filteredLines = append(filteredLines[:j-1], filteredLines[j+1:]...)
+			}
+		}
+	}
+	return co2
 }
 
 func binaryToDecimal(num int) int {
---
+++ 2021/day03/part1/main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+)
+
+//go:embed input.txt
+var input string
+
+func main() {
+	lines := strings.Split(strings.TrimSpace(input), "\n")
+
+	gamma := gamma(lines)
+	epsilon := epsilon(lines)
+
+	gammaInt, _ := strconv.Atoi(string(gamma))
+	epsilonInt, _ := strconv.Atoi(string(epsilon))
+
+	power := binaryToDecimal(gammaInt) * binaryToDecimal(epsilonInt)
+
+	fmt.Println(gammaInt, epsilonInt, power)
+}
+
+func gamma(lines []string) []byte {
+	gamma := make([]byte, len(lines[0]))
+
+	for i := 0; i < len(lines[0]); i++ {
+		ones := 0
+		zeroes := 0
+
+		for j := 0; j < len(lines); j++ {
+			if string(lines[j][i]) == "0" {
+				zeroes++
+			}
+			if string(lines[j][i]) == "1" {
+				ones++
+			}
+		}
+
+		if zeroes > ones {
+			gamma[i] = '0'
+		}
+		if ones > zeroes {
+			gamma[i] = '1'
+		}
+	}
+
+	fmt.Println(string(gamma))
+	return gamma
+}
+
+func epsilon(lines []string) []byte {
+	epsilon := make([]byte, len(lines[0]))
+
+	for i := 0; i < len(lines[0]); i++ {
+		ones := 0
+		zeroes := 0
+
+		for j := 0; j < len(lines); j++ {
+			if string(lines[j][i]) == "0" {
+				zeroes++
+			}
+			if string(lines[j][i]) == "1" {
+				ones++
+			}
+		}
+
+		if zeroes > ones {
+			epsilon[i] = '1'
+		}
+		if ones > zeroes {
+			epsilon[i] = '0'
+		}
+	}
+
+	fmt.Println(string(epsilon))
+	return epsilon
+}
+
+func binaryToDecimal(num int) int {
+	var remainder int
+	index := 0
+	dec := 0
+	for num != 0 {
+		remainder = num % 10
+		num = num / 10
+		dec = dec + remainder*int(math.Pow(2, float64(index)))
+		index++
+	}
+	return dec
+}
